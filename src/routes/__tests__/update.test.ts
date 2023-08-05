1cba365968e8892d8961aaa9e5a2a083518004ab
Aman Singh <amansinghs7010@gmail.com>
2023-08-04 23:11:16 -0400

    throw error if a user tries to edit a reserved ticket

--- src/routes/__tests__/update.test.ts
+++ src/routes/__tests__/update.test.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 
 import { app } from '../../app';
 import { natsWrapper } from '../../nats-wrapper';
+import { Ticket } from '../../models/ticket';
 
 it('returns a 404 if the provided id does not exists', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -124,4 +125,29 @@ it('publishes an event', async () => {
     .expect(200);
 
   expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it('cannot edit a reserved ticket', async () => {
+  const cookie = global.getAuthCookie();
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'A Title',
+      price: 10,
+    });
+
+  const ticket = await Ticket.findById(response.body.id);
+  ticket!.set({ orderId: new mongoose.Types.ObjectId().toHexString() });
+  await ticket!.save();
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'Updated Title',
+      price: 5,
+    })
+    .expect(400);
 });
